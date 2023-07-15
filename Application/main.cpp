fd743d2e1cc8efabedac83828782081845033edc
Rei-sun <reiiii0109@gmail.com>
2023-07-16 08:42:46 +0900

    Chenge main.cpp

--- Application/main.cpp
+++ Application/main.cpp
@@ -27,18 +27,21 @@ int main(){
 
     std::string target = "/dev/ESP32-WROOM-32E";
     SerialCommunication serial;
-    serial.RegisterCallbackOnConnect([](){printf("いぇーい、接続できたよぉぉぉ！\n");});
-    serial.RegisterCallbackOnDisconnect([](){printf("いぇーい、切断したよぉぉぉ！\n");});
-    serial.RegisterCallbackOnDisconnected([](){printf("接続が切れましたが？\n");});
-    serial.RegisterCallbackOnReconnected([](){printf("いぇーい、再接続されましたぁぁぁ!\n");});
+    serial.RegisterCallbackOnConnect([](){ printf("いぇーい、接続できたよぉぉぉ！\n"); });
+    serial.RegisterCallbackOnDisconnect([](){ printf("いぇーい、切断したよぉぉぉ！\n"); });
+    serial.RegisterCallbackOnReceive([](void* data, size_t size){ printf("Recv %ld byte, %s\n", size, (char *)data); });
+    serial.RegisterCallbackOnDisconnected([](){ printf("接続が切れましたが？\n"); });
+    serial.RegisterCallbackOnReconnected([](){ printf("いぇーい、再接続されましたぁぁぁ!\n"); });
 
-    if (!serial.Open(target)) {
+    if (!serial.Open(target, false)) {
         std::cout << "Cannot connect " << target << std::endl;
         std::quick_exit(0);
     }
 
-    for(;;) {
-        serial.Transmit("test");
+    for(int i;;i++) {
+        char buffer[32];
+        sprintf(buffer, "[%03d] 12345678901234567890", i);
+        serial.Transmit(buffer);
         sleep(1);
     }
 
