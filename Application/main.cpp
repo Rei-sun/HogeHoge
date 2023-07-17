18b4ed65f1d9d3575a6e2ace4d52078f41bc6027
Rei-sun <reiiii0109@gmail.com>
2023-07-18 00:27:15 +0900

    Change HogeHoge

--- Application/main.cpp
+++ Application/main.cpp
@@ -11,8 +11,9 @@ int main(){
     timer.SetHz(1);
 
     while (Hoge::Good()) {
-        uint8_t test_data[] = { 3, 6, 9, 12 };
-        Hoge::motor_control_module_1.Command(1, 1, 4, test_data);
+        Hoge::motor_control_module_1.SetDuty(1, 50.0f);
+        Hoge::motor_control_module_1.SendBatch();
+        printf("duty = %f\n", Hoge::motor_control_module_1.GetDuty(1));
         printf("delta time = %f\n", timer.GetDeltaTime());
         timer.Sleep();
     }
