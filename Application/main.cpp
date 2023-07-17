3f6d4c53bf37d7d957943da85e7e14648e23854d
Rei-sun <reiiii0109@gmail.com>
2023-07-18 01:18:57 +0900

    Change HogeHoge

--- Application/main.cpp
+++ Application/main.cpp
@@ -8,13 +8,19 @@ using namespace HogeHoge;
 
 int main(){
     Timer timer;
-    timer.SetHz(1);
+    timer.SetHz(1000);
 
     while (Hoge::Good()) {
-        Hoge::motor_control_module_1.SetDuty(1, 50.0f);
-        Hoge::motor_control_module_1.SendBatch();
+        Hoge::encoder_module_1.SendGetLocalization();
+        Hoge::encoder_module_1.SendGetAllPulse();
+        Hoge::sensor_module_1.SendGetSensorData();
+
         printf("duty = %f\n", Hoge::motor_control_module_1.GetDuty(1));
         printf("delta time = %f\n", timer.GetDeltaTime());
+        
+        Hoge::motor_control_module_1.SendBatch();
+        Hoge::solenoid_module_1.SendBatch();
+        
         timer.Sleep();
     }
 
