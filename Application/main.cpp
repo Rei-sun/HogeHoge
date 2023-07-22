dd2595ae5fa844ebe3f891be8fc5503f97d35576
Rei-sun <reiiii0109@gmail.com>
2023-07-22 12:45:04 +0900

    HogeHoge

--- Application/main.cpp
+++ Application/main.cpp
@@ -7,20 +7,33 @@
 using namespace HogeHoge;
 
 int main(){
+    printf("pid = %d\n", getpid());
+
     Timer timer;
-    timer.SetHz(1000);
+    timer.SetHz(100);
+
+    // start measurement time point
+    std::chrono::system_clock::time_point start;
+    
+    // end measurement time point
+    std::chrono::system_clock::time_point end;
 
     while (Hoge::Good()) {
+        start = std::chrono::system_clock::now();
         Hoge::encoder_module_1.SendGetLocalization();
         Hoge::encoder_module_1.SendGetAllPulse();
         Hoge::sensor_module_1.SendGetSensorData();
 
-        printf("duty = %f\n", Hoge::motor_control_module_1.GetDuty(1));
-        printf("delta time = %f\n", timer.GetDeltaTime());
+        // printf("duty = %f\n", Hoge::motor_control_module_1.GetDuty(1));
+        // printf("delta time = %f\n", timer.GetDeltaTime());
         
         Hoge::motor_control_module_1.SendBatch();
         Hoge::solenoid_module_1.SendBatch();
-        
+
+        end = std::chrono::system_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
+        std::cout << "[" << std::chrono::duration_cast<std::chrono::milliseconds>(end.time_since_epoch()).count() << "]" << "processing time = " << elapsed << std::endl;
+
         timer.Sleep();
     }
 
