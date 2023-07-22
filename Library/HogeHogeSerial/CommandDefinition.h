a706dd2fa211b528044145990eac4f94e560fcf6
Rei-sun <reiiii0109@gmail.com>
2023-07-22 12:42:12 +0900

    move

--- Library/HogeHogeSerial/CommandDefinition.h
+++ Library/HogeHogeSerial/CommandDefinition.h
@@ -2,19 +2,27 @@
 
 namespace HogeHoge {
     enum class CMD_MotorControlModule {
+        Invalid,
+        OK,
         SetAllDuty
     };
 
     enum class CMD_EncoderModule {
+        Invalid,
+        OK,
         GetLocalization,
         GetAllPulse
     };
 
     enum class CMD_SensorModule {
+        Invalid,
+        OK,
         GetSensorData
     };
 
     enum class CMD_SolenoidModule {
+        Invalid,
+        OK,
         SetAllState,
         SetState
     };
