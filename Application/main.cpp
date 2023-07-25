c66f8c574602723e0d645ffbb4dd433689b54dda
Rei-sun <reiiii0109@gmail.com>
2023-07-25 23:42:58 +0900

    Hoge~

--- Application/main.cpp
+++ Application/main.cpp
@@ -26,6 +26,17 @@ int main(){
 
         // printf("duty = %f\n", Hoge::motor_control_module_1.GetDuty(1));
         // printf("delta time = %f\n", timer.GetDeltaTime());
+
+        float potentio = Hoge::sensor_module_1.GetAnalog(1);
+        float duty_potentio = potentio / 4095.f * 100.f;
+
+        float duty_roll = Hoge::encoder_module_1.GetEulerRoll() / 90.0f * 100.f;
+
+        bool button = Hoge::sensor_module_1.GetSwitch(1);
+
+        Hoge::motor_control_module_1.SetDuty(1, abs(duty_roll));
+        Hoge::solenoid_module_1.SetState(1, button);
+
         
         Hoge::motor_control_module_1.SendBatch();
         Hoge::solenoid_module_1.SendBatch();
