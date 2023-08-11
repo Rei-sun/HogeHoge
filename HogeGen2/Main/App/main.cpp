aada554899c96405108b8e566113e75d0af04bfc
Rei-sun <reiiii0109@gmail.com>
2023-08-11 17:58:28 +0900

    Hoge

--- HogeGen2/Main/App/main.cpp
+++ HogeGen2/Main/App/main.cpp
@@ -1,41 +1,46 @@
+#include <Application.h>
 #include <HogeHoge.h>
-
-#include <iostream>
-#include <string>
-#include <signal.h>
-
-#include <GUICommunication/GUICommunication.h>
 
 using namespace HogeGen2;
 
 int main(){
     Hoge::Init();
-    //GUICommunication gcommu(20000);
-    printf("pid = %d\n", getpid());
+
+    //-----------------------------
+    // Setup Start
+    //-----------------------------
+    Application app;
+    app.Setup();
+
+    //-----------------------------
+    // Setup End
+    //-----------------------------
 
     Timer timer;
     timer.SetHz(100);
 
-    // start measurement time point
-    std::chrono::system_clock::time_point start;
-    
-    // end measurement time point
-    std::chrono::system_clock::time_point end;
-
     while (Hoge::Good()) {
-        start = std::chrono::system_clock::now();
 
+        // Request sensor value
         Hoge::GetSensorValue();
-        
-        printf("p %d\n", ModuleManagerMain::sensorModules[0]->GetAnalog(1));
 
+        //-----------------------------
+        // Loop Start
+        //-----------------------------
+        app.Loop();
+
+        //-----------------------------
+        // Loop End
+        //-----------------------------
+
+        // Set actuator control
         Hoge::SetActuatorControl();
 
-        end = std::chrono::system_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end-start).count();
-        std::cout << "[" << std::chrono::duration_cast<std::chrono::milliseconds>(end.time_since_epoch()).count() << "]" << "processing time = " << elapsed << std::endl;
+        printf("%f\n", timer.GetProcessTime());
 
+        // Sleep for loop
         timer.Sleep();
+
     }
 
     return 0;
